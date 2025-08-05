24c58013944b4f7acba4e621f153abd402489e4b
Алексей Содномов <sodnomoff@gmail.com>
2025-08-06 00:18:53 +0800

    fix: small fixes

--- uni_links/ios/Classes/UniLinksPlugin.h
+++
@@ -1,7 +0,0 @@
-#import <Flutter/Flutter.h>
-
-/// Flutter plugin for handling universal links and custom URL schemes on iOS.
-/// This plugin supports both Universal Links (https) and Custom URL schemes.
-@interface UniLinksPlugin : NSObject <FlutterPlugin, FlutterStreamHandler>
-
-@end
